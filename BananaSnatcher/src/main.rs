3e465dbe574ca0a62916d50de4c081b8638744de
Sebabacou <sebastien.bertilsouchet@protonmail.com>
2024-11-05 13:56:21 +0100

    FIX: LICENSE labels: MonkeySpy
    fix LICENSE for a protective license

--- BananaSnatcher/src/main.rs
+++ BananaSnatcher/src/main.rs
@@ -1,6 +1,12 @@
 mod key_catcher;
 mod user_security;
 
+#[cfg(target_os = "windows")]
+fn main() {
+    eprintln!("BananaSnatcher is still in development for Windows");
+}
+
+#[cfg(target_os = "linux")]
 fn main() {
     if !user_security::ask_for_confirmation() {
         return;
